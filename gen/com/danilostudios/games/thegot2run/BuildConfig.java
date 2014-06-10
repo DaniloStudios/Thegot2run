6437cdeb1fb52b29395d8885df95d05672fe79e8
Danilo <danmestrado@yahoo.com.br>
2014-06-10 19:01:20 -0300

    The Got 2 Run
    Outro jogo para android baseado em java

---
+++ gen/com/danilostudios/games/thegot2run/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.danilostudios.games.thegot2run;
+
+public final class BuildConfig 
+{
+    public final static boolean DEBUG = true;
+}
